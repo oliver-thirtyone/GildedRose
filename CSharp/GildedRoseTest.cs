81742e1fb4a2f1fae3569f2470f95f8be60ba974
Oliver S <r3zn1k@bluewin.ch>
2020-11-27 16:38:05 +0100

    Initial commit

---
+++ CSharp/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace GildedRose
+{
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> items = new List<Item> {new Item {Name = "foo", SellIn = 0, Quality = 0}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        }
+    }
+}
