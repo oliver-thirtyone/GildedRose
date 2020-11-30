29157a866268deeef4e0ad0a7f37d0efba7db954
Oliver S <r3zn1k@bluewin.ch>
2020-11-30 19:29:05 +0100

    r add empty if-else-statement

--- CSharp/GildedRose.cs
+++ CSharp/GildedRose.cs
@@ -27,6 +27,15 @@ namespace GildedRose
                 return;
             }
 
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                
+            }
+            else
+            {
+                
+            }
+            
             UpdateQualityForEverythingElse(item);
             return;
         }
