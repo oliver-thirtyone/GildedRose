e62f61c63f0bfa3c59e9d7d44a2fad17a431726c
Oliver S <r3zn1k@bluewin.ch>
2020-11-30 19:20:13 +0100

    r remove unnecessary check in if statement

--- CSharp/GildedRose.cs
+++ CSharp/GildedRose.cs
@@ -45,7 +45,7 @@ namespace GildedRose
             }
             else
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (item.Quality > 0)
                     {
