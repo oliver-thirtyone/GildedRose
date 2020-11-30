bbd97f17cdd93e2e3d932c3b83d8c4d41b7b0be0
Oliver S <r3zn1k@bluewin.ch>
2020-11-30 17:19:07 +0100

    r extract method DoStuff

--- CSharp/GildedRoseTest.cs
+++ CSharp/GildedRoseTest.cs
@@ -11,10 +11,17 @@ namespace GildedRose
         [Test]
         public void foo()
         {
+            var result = DoStuff();
+            Approvals.Verify(result);
+        }
+
+        private static string DoStuff()
+        {
             IList<Item> items = new List<Item> {new Item {Name = "foo", SellIn = 0, Quality = 0}};
             var app = new GildedRose(items);
             app.UpdateQuality();
-            Approvals.Verify(items[0].ToString());
+            var result = items[0].ToString();
+            return result;
         }
     }
 }
