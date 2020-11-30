eda70641b9878a547c11cb45a355a9147804f99c
Oliver S <r3zn1k@bluewin.ch>
2020-11-30 19:33:10 +0100

    r delete grey lines

--- CSharp/GildedRose.cs
+++ CSharp/GildedRose.cs
@@ -31,13 +31,6 @@ namespace GildedRose
             {
                 if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
                 }
                 else
                 {
@@ -75,13 +68,6 @@ namespace GildedRose
                 {
                     if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
                     }
                     else
                     {
@@ -103,32 +89,6 @@ namespace GildedRose
                         }
                     }
                 }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
@@ -146,10 +106,6 @@ namespace GildedRose
                                 item.Quality = item.Quality - 1;
                             }
                         }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
                     }
                 }
 
