8ddb7e95c5fe8433e3cf4b89b1bf9320a9fb6f49
Oliver S <r3zn1k@bluewin.ch>
2020-11-30 19:34:15 +0100

    r remove first unused if statement

--- CSharp/GildedRose.cs
+++ CSharp/GildedRose.cs
@@ -29,31 +29,25 @@ namespace GildedRose
 
             if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality < 50)
                 {
-                }
-                else
-                {
-                    if (item.Quality < 50)
+                    item.Quality = item.Quality + 1;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.SellIn < 11)
                         {
-                            if (item.SellIn < 11)
+                            if (item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
+                        }
 
-                            if (item.SellIn < 6)
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
