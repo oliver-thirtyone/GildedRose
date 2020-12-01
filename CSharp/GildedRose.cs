02fc8955aa24aeeba66b6f8b3d0c83d5e4a9c57a
Oliver S <r3zn1k@bluewin.ch>
2020-12-01 11:21:11 +0100

    r remove stupid if-else-statements

--- CSharp/GildedRose.cs
+++ CSharp/GildedRose.cs
@@ -33,22 +33,19 @@ namespace GildedRose
                 {
                     item.Quality = item.Quality + 1;
 
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.SellIn < 11)
                     {
-                        if (item.SellIn < 11)
+                        if (item.Quality < 50)
                         {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
+                            item.Quality = item.Quality + 1;
                         }
+                    }
 
-                        if (item.SellIn < 6)
+                    if (item.SellIn < 6)
+                    {
+                        if (item.Quality < 50)
                         {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
+                            item.Quality = item.Quality + 1;
                         }
                     }
                 }
