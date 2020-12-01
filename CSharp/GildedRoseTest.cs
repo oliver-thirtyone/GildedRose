10b9120129504d9e8b7a26a04a66c915fad992a0
Oliver S <r3zn1k@bluewin.ch>
2020-12-01 10:51:38 +0100

    t add "Aged Brie" and "Backstage passes to a TAFKAL80ETC concert"

--- CSharp/GildedRoseTest.cs
+++ CSharp/GildedRoseTest.cs
@@ -11,7 +11,7 @@ namespace GildedRose
         [Test]
         public void foo()
         {
-            var names = new List<string> {"foo"};
+            var names = new List<string> {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"};
             CombinationApprovals.VerifyAllCombinations(DoStuff, names);
         }
 
