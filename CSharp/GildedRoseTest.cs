7aac39af00930ffb0b96619ac3e3eb3e31a9609e
Oliver S <r3zn1k@bluewin.ch>
2020-12-01 10:58:32 +0100

    t add parameter sellIns

--- CSharp/GildedRoseTest.cs
+++ CSharp/GildedRoseTest.cs
@@ -12,13 +12,14 @@ namespace GildedRose
         public void foo()
         {
             var names = new List<string> {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+            var sellIns = new List<int> {-1, 0, 1};
             var qualities = new List<int> {-1, 0, 1};
-            CombinationApprovals.VerifyAllCombinations(DoStuff, names, qualities);
+            CombinationApprovals.VerifyAllCombinations(DoStuff, names, sellIns, qualities);
         }
 
-        private static string DoStuff(string name, int quality)
+        private static string DoStuff(string name, int sellIn, int quality)
         {
-            IList<Item> items = new List<Item> {new Item {Name = name, SellIn = 0, Quality = quality}};
+            IList<Item> items = new List<Item> {new Item {Name = name, SellIn = sellIn, Quality = quality}};
             var app = new GildedRose(items);
             app.UpdateQuality();
             var result = items[0].ToString();
